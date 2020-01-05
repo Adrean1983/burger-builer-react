856c4a0e82431ec709f6201c8e7bee9755191768
Adrean1983 <adrean_@hotmail.com>
2020-01-05 14:22:42 -0500

    second exercise - add proptypes to burger ingredients

--- src/Steps/steps.js
+++ src/Steps/steps.js
@@ -1,3 +1,10 @@
 const stepOne = {
   // Write a switch statement in BurgerIngredient gets a future prop to be added, props.type. Switch on the ingredients from the CSS file and return a div
 };
+
+const stepTwo = {
+  // Add prop type validation to App. npm install --save prop-types
+  // switch (props.type) <-- Add validation so that this prop is validated.
+  // Make the BurgerIngredient a class component.
+  // Add the PropType validtion
+};
